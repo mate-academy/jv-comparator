9cfb57f1dd0e6be871bfbf0f6a1dac4aa15631f9
IvanLuchkin <id.luchkin@gmail.com>
2020-12-14 20:34:36 +0200

    Comparator moved to field variable

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,18 +6,20 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-    public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        Set<Employee> sortedSet = new TreeSet<>(new Comparator<>() {
-            @Override
-            public int compare(Employee employee1, Employee employee2) {
-                int namesComparisonResult =
-                        Integer.compare(employee1.getAge(), employee2.getAge());
-                if (namesComparisonResult != 0) {
-                    return namesComparisonResult;
-                }
-                return employee1.getName().compareTo(employee2.getName());
+    private final Comparator<Employee> ageNameComparator = new Comparator<>() {
+        @Override
+        public int compare(Employee employee1, Employee employee2) {
+            int namesComparisonResult =
+                    Integer.compare(employee1.getAge(), employee2.getAge());
+            if (namesComparisonResult != 0) {
+                return namesComparisonResult;
             }
-        });
+            return employee1.getName().compareTo(employee2.getName());
+        }
+    };
+
+    public Set<Employee> getEmployByOrder(List<Employee> employees) {
+        Set<Employee> sortedSet = new TreeSet<>(ageNameComparator);
         sortedSet.addAll(employees);
         return sortedSet;
     }
