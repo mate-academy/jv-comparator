2cdfa5835328ddbe0276ed914e89fce665613192
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-15 17:36:04 +0200

    added comparator to EmployeeService

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,9 +1,21 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
 public class EmployeeService {
+    private Comparator<Employee> injusticeComparator = new Comparator<Employee>() {
+        @Override
+        public int compare(Employee employee1, Employee employee2) {
+            int compareAges = Integer.compare(employee1.getAge(), employee2.getAge());
+            if (compareAges != 0) {
+                return compareAges;
+            }
+            return employee1.getName().toLowerCase().compareTo(employee2.getName().toLowerCase());
+        }
+    };
+
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
         return null;
     }
