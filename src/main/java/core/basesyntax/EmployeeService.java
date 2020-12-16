2cdca4b324d58e789eca63bae7839a9efe641cdf
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-16 14:59:39 +0200

    Comparator moved to getEmployByOrder method

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,19 +6,20 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-    public static final Comparator<Employee> INJUSTICE_COMPARATOR = new Comparator<Employee>() {
-        @Override
-        public int compare(Employee employee1, Employee employee2) {
-            int compareAges = Integer.compare(employee1.getAge(), employee2.getAge());
-            if (compareAges != 0) {
-                return compareAges;
-            }
-            return employee1.getName().toLowerCase().compareTo(employee2.getName().toLowerCase());
-        }
-    };
 
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        Set<Employee> employeesSorted = new TreeSet<>(INJUSTICE_COMPARATOR);
+        Comparator<Employee> injusticeComparator = new Comparator<Employee>() {
+            @Override
+            public int compare(Employee employee1, Employee employee2) {
+                int compareAges = Integer.compare(employee1.getAge(), employee2.getAge());
+                if (compareAges != 0) {
+                    return compareAges;
+                }
+                return employee1.getName().toLowerCase()
+                        .compareTo(employee2.getName().toLowerCase());
+            }
+        };
+        Set<Employee> employeesSorted = new TreeSet<>(injusticeComparator);
         employeesSorted.addAll(employees);
         return employeesSorted;
     }
