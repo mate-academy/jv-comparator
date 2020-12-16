88b9d2bcac70f004e3ad3642e8a97c496d88832d
bogdan <b.kostevich@gmail.com>
2020-12-16 14:11:35 +0200

    Realized own sort using class Comparator.

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,24 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Comparator<Employee> comparator = new Comparator<Employee>() {
+            @Override
+            public int compare(Employee firstEmployee, Employee secondEmployee) {
+                int compare = Integer.compare(firstEmployee.getAge(), secondEmployee.getAge());
+                if (compare != 0) {
+                    return compare;
+                }
+                return firstEmployee.getName().compareTo(secondEmployee.getName());
+            }
+        };
+        Set<Employee> allEmployees = new TreeSet<>(comparator);
+        allEmployees.addAll(employees);
+        return allEmployees;
     }
 }
