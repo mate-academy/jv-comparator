ea0ce8661afbe52c492d186ba9222459f8269e07
IvanLuchkin <id.luchkin@gmail.com>
2020-12-17 20:55:19 +0200

    Comparator moved back to TreeSet constructor

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,20 +6,19 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-    private final Comparator<Employee> ageNameComparator = new Comparator<>() {
-        @Override
-        public int compare(Employee employee1, Employee employee2) {
-            int namesComparisonResult =
-                    Integer.compare(employee1.getAge(), employee2.getAge());
-            if (namesComparisonResult != 0) {
-                return namesComparisonResult;
-            }
-            return employee1.getName().compareTo(employee2.getName());
-        }
-    };
 
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        Set<Employee> sortedSet = new TreeSet<>(ageNameComparator);
+        Set<Employee> sortedSet = new TreeSet<>(new Comparator<>() {
+            @Override
+            public int compare(Employee employee1, Employee employee2) {
+                int namesComparisonResult =
+                        Integer.compare(employee1.getAge(), employee2.getAge());
+                if (namesComparisonResult != 0) {
+                    return namesComparisonResult;
+                }
+                return employee1.getName().compareTo(employee2.getName());
+            }
+        });
         sortedSet.addAll(employees);
         return sortedSet;
     }
