fc2eea1be8e291dc6d665241c74dc007821b08e0
Resci <vIsUAl123>
2020-12-20 12:07:42 +0200

    Implement comparator for Employee class

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,24 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
+    private final Comparator<Employee> comparator = new Comparator<>() {
+        @Override
+        public int compare(Employee firstEmployee, Employee secondEmployee) {
+                if (firstEmployee.getAge() != secondEmployee.getAge()) {
+                    return firstEmployee.getAge() - secondEmployee.getAge();
+                }
+                return firstEmployee.getName().compareTo(secondEmployee.getName());
+        }
+    };
+
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Set<Employee> employeeSet = new TreeSet<>(comparator);
+        employeeSet.addAll(employees);
+        return employeeSet;
     }
 }
