7c200029593d41e86a1d5ba227d83648d97eca4a
kerrender <agreed_note@hotmail.com>
2020-12-21 17:41:28 +0200

    Initial

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,25 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
+    Comparator<Employee> employeeComparator = new Comparator<Employee>() {
+        @Override
+        public int compare(Employee employee1, Employee employee2) {
+            int comparedAges = Integer.compare(employee1.getAge(), employee2.getAge());
+            if (comparedAges != 0) {
+                return comparedAges;
+            }
+            return employee1.getName().compareTo(employee2.getName());
+        }
+    };
+
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Set<Employee> orderedSet = new TreeSet<>(employeeComparator);
+        orderedSet.addAll(employees);
+        return orderedSet;
     }
 }
