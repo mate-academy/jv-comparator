8485d09b4adc210318bedf4ace7a3841e680286c
Tonitrus <grom4er@gmail.com>
2020-12-21 15:12:00 +0200

    Complete. Test passed

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,11 +1,25 @@
 package core.basesyntax;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-
-        return null;
+        Comparator<Employee> employeeComparator = new Comparator<>() {
+            @Override
+            public int compare(Employee employeeOne, Employee employeeTwo) {
+                int checkAge = Integer.compare(employeeOne.getAge(), employeeTwo.getAge());
+                if (checkAge != 0) {
+                    return checkAge;
+                }
+                return employeeOne.getName().compareTo(employeeTwo.getName());
+            }
+        };
+        Set<Employee> employeeSet = new TreeSet<>(employeeComparator);
+        employeeSet.addAll(employees);
+        return employeeSet;
     }
 
 }
