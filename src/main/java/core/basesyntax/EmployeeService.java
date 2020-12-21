faaf394b0bb362980df9eba5e9912b263c0c7262
ivanluchkin <id.luchkin@gmail.com>
2020-12-21 17:13:58 +0200

    Variable name changed, redundant line removed

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,15 +6,14 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
         Set<Employee> sortedSet = new TreeSet<>(new Comparator<>() {
             @Override
             public int compare(Employee employee1, Employee employee2) {
-                int namesComparisonResult =
+                int ageComparisonResult =
                         Integer.compare(employee1.getAge(), employee2.getAge());
-                if (namesComparisonResult != 0) {
-                    return namesComparisonResult;
+                if (ageComparisonResult != 0) {
+                    return ageComparisonResult;
                 }
                 return employee1.getName().compareTo(employee2.getName());
             }
