3bd64a0337dbd6b2cc0772a44bfe4036bcdaafbd
Resci <vIsUAl123>
2020-12-21 15:33:03 +0200

    Move comparator to method

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,17 +6,18 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-    private final Comparator<Employee> comparator = new Comparator<>() {
-        @Override
-        public int compare(Employee firstEmployee, Employee secondEmployee) {
+
+    public Set<Employee> getEmployByOrder(List<Employee> employees) {
+        Comparator<Employee> comparator = new Comparator<>() {
+            @Override
+            public int compare(Employee firstEmployee, Employee secondEmployee) {
                 if (firstEmployee.getAge() != secondEmployee.getAge()) {
                     return firstEmployee.getAge() - secondEmployee.getAge();
                 }
                 return firstEmployee.getName().compareTo(secondEmployee.getName());
-        }
-    };
+            }
+        };
 
-    public Set<Employee> getEmployByOrder(List<Employee> employees) {
         Set<Employee> employeeSet = new TreeSet<>(comparator);
         employeeSet.addAll(employees);
         return employeeSet;
