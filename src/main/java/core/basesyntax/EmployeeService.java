24c55582ea68fc9388e3d3ff5c348564d4d0686d
Alekearl <alexandrhurko@gmail.com>
2020-12-22 21:19:23 +0200

    created getEmployByOrder method in class EmployeeService

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,26 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
+    Set<Employee> employeeSet;
+
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Comparator<Employee> employeeComparator = new Comparator<Employee>() {
+            @Override
+            public int compare(Employee employeeOne, Employee employeeTwo) {
+                int compareEmployee = Integer.compare(employeeOne.getAge(), employeeTwo.getAge());
+                if (compareEmployee != 0) {
+                    return compareEmployee;
+                }
+                return employeeOne.getName().compareTo(employeeTwo.getName());
+            }
+        };
+        employeeSet = new TreeSet<>(employeeComparator);
+        employeeSet.addAll(employees);
+        return employeeSet;
     }
 }
