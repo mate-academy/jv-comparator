5155c3019fffb778dc2505f908b37fd0df4b4843
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-12-22 13:03:52 +0200

    first commit

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -2,9 +2,14 @@ package core.basesyntax;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
+    private EmployeeComparator employeeComparator = new EmployeeComparator();
+
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Set<Employee> sortedEmployee = new TreeSet<>(employeeComparator);
+        sortedEmployee.addAll(employees);
+        return sortedEmployee;
     }
 }
