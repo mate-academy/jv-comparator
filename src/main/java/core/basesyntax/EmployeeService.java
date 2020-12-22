ac72f8de3546be62a35e74e23bdea655ad02f5ab
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2020-12-22 19:36:49 +0200

    Added comparator

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,21 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
+    Comparator<Employee> employeeComparator = (employee1, employee2) -> {
+        if (employee1.getAge() != employee2.getAge()) {
+            return Integer.compare(employee1.getAge(), employee2.getAge());
+        }
+        return employee1.getName().compareTo(employee2.getName());
+    };
+
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Set<Employee> employeeSet = new TreeSet<>(employeeComparator);
+        employeeSet.addAll(employees);
+        return employeeSet;
     }
 }
