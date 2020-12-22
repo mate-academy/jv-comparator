e72958411df8c331a2964aafc4fd5cd37a731dd3
Igor Bochkor <igorbochkor@gmail.com>
2020-12-22 16:14:30 +0200

    fixed code style

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,17 +6,13 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-    Comparator<Employee> comparator = (employee1, employee2) -> {
-        if (employee1.getAge() != employee2.getAge()) {
-            return Integer.compare(employee1.getAge(), employee2.getAge());
-        }
-        return employee1.getName().compareTo(employee2.getName());
-    };
-
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        if (employees == null || employees.size() == 0) {
-            return new TreeSet<>();
-        }
+        Comparator<Employee> comparator = (employee1, employee2) -> {
+            if (employee2.getAge() != employee1.getAge()) {
+                return Integer.compare(employee1.getAge(), employee2.getAge());
+            }
+            return employee1.getName().compareTo(employee2.getName());
+        };
         Set<Employee> employeeSet = new TreeSet<>(comparator);
         employeeSet.addAll(employees);
         return employeeSet;
