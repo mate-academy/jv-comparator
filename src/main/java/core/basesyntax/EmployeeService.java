3ffa5dcf28733294b1abb61a1535f905c52074d1
Yaroslav <yaroslav8oroboro@gmail.com>
2020-12-22 18:39:30 +0200

    added Comparator implementation

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,25 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
+    private Comparator<Employee> employeeComparator = new Comparator<Employee>() {
+        @Override
+        public int compare(Employee employee1, Employee employee2) {
+            int compare = Integer.compare(employee1.getAge(), employee2.getAge());
+            if (compare != 0) {
+                return compare;
+            }
+            return employee1.getName().compareTo(employee2.getName());
+        }
+    };
+
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Set<Employee> employeeSet = new TreeSet<>(employeeComparator);
+        employeeSet.addAll(employees);
+        return employeeSet;
     }
 }
