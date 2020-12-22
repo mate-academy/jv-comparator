4c41028f992121285b9ce1ee936c48acbecb03c8
ShemsheyMaxim <shanhaymax@gmail.com>
2020-12-22 18:46:32 +0200

    added logic to methods getEmployByOrder()

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,24 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Comparator<Employee> employeeComparator = new Comparator<Employee>() {
+            @Override
+            public int compare(Employee employee1, Employee employee2) {
+                int compareAge = Integer.compare(employee1.getAge(), employee2.getAge());
+                if (compareAge != 0) {
+                    return compareAge;
+                }
+                return employee1.getName().compareTo(employee2.getName());
+            }
+        };
+        TreeSet<Employee> sortedEmployee = new TreeSet<>(employeeComparator);
+        sortedEmployee.addAll(employees);
+        return sortedEmployee;
     }
 }
