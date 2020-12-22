e6f4848114e229fe13ea9a7dea089d644f15dfa6
Likh-Alex <likhachov991@protonmail.com>
2020-12-22 15:56:53 +0200

    removed `parametrized` type in `new Comparator<>` as it is redundant

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -7,7 +7,7 @@ import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        Comparator<Employee> employeeComparator = new Comparator<Employee>() {
+        Comparator<Employee> employeeComparator = new Comparator<>() {
             @Override
             public int compare(Employee employee1, Employee employee2) {
                 int comparedAges = Integer.compare(employee1.getAge(), employee2.getAge());
