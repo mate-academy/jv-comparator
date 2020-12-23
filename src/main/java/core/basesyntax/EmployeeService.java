dc55b2f71f98b3228aab957767dc24363a17f1c3
antonpanchishko <PanchishkoMayor@gmail.com>
2020-12-23 16:21:35 +0200

    1) created custom comparator

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,24 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Comparator<Employee> employeeComparator = new Comparator<Employee>() {
+            @Override
+            public int compare(Employee firstEmployee, Employee secondEmployee) {
+                int comparedAge = Integer.compare(firstEmployee.getAge(), secondEmployee.getAge());
+                if (comparedAge != 0) {
+                    return comparedAge;
+                }
+                return firstEmployee.getName().compareTo(secondEmployee.getName());
+            }
+        };
+        Set<Employee> sortedEmployeeSet = new TreeSet<>(employeeComparator);
+        sortedEmployeeSet.addAll(employees);
+        return sortedEmployeeSet;
     }
 }
