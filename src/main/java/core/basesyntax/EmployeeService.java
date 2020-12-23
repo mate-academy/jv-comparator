f5f495eb6b9e02e0f4c0a63b3e694850276ffb94
Andrii Bardin <andrii.bardin@gmail.com>
2020-12-23 08:15:11 +0200

    created new branch

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -7,7 +7,7 @@ import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        Comparator <Employee> comparator = new Comparator<>() {
+        Comparator<Employee> comparator = new Comparator<>() {
             @Override
             public int compare(Employee employee1, Employee employee2) {
                 int compareAge = Integer.compare(employee1.getAge(),
