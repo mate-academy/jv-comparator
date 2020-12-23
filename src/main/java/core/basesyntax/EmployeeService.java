f2ebdffeacd0e52886f1af02a504bd9cdc2e7a5b
tbondare <tanya.bon2016@gmail.com>
2020-12-23 11:12:04 +0200

    redundant empty line, local variable & type in constructor was delet

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,16 +6,14 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-
-    Comparator<Employee> employeeComparator = new Comparator<Employee>() {
+    Comparator<Employee> employeeComparator = new Comparator<>() {
         @Override
         public int compare(Employee employee1, Employee employee2) {
             int compareAges = Integer.compare(employee1.getAge(), employee2.getAge());
             if (compareAges != 0) {
                 return compareAges;
             }
-            int compareName = employee1.getName().compareTo(employee2.getName());
-            return compareName;
+            return employee1.getName().compareTo(employee2.getName());
         }
     };
 
