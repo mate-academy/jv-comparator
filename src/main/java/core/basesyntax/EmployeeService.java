511ce9dcf31dd6caa475c504bd17776a877e3d69
Sashk52 <a.tsven@gmail.com>
2020-12-23 12:19:48 +0200

    moved comparator inside method getEmployOrder

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,19 +6,19 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-    Comparator<Employee> employeeComparator = new Comparator<Employee>() {
-        @Override
-        public int compare(Employee firstEmployee, Employee secondEmployee) {
-            int howIsYonger = Integer.compare(firstEmployee.getAge(), secondEmployee.getAge());
-            if (howIsYonger != 0) {
-                return howIsYonger;
-            }
-            return firstEmployee.getName().compareTo(secondEmployee.getName());
-        }
-
-    };
 
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
+        Comparator<Employee> employeeComparator = new Comparator<Employee>() {
+            @Override
+            public int compare(Employee firstEmployee, Employee secondEmployee) {
+                int howIsYonger = Integer.compare(firstEmployee.getAge(), secondEmployee.getAge());
+                if (howIsYonger != 0) {
+                    return howIsYonger;
+                }
+                return firstEmployee.getName().compareTo(secondEmployee.getName());
+            }
+
+        };
         Set<Employee> resultSet = new TreeSet<>(employeeComparator);
         resultSet.addAll(employees);
         return resultSet;
