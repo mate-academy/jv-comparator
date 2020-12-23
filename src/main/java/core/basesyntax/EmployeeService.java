9722dd6260878daa27caf735ccb4d2b2d5c2d6df
tbondare <tanya.bon2016@gmail.com>
2020-12-23 11:51:00 +0200

    employeeComporator is local variable

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,18 +6,18 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-    Comparator<Employee> employeeComparator = new Comparator<>() {
-        @Override
-        public int compare(Employee employee1, Employee employee2) {
-            int compareAges = Integer.compare(employee1.getAge(), employee2.getAge());
-            if (compareAges != 0) {
-                return compareAges;
-            }
-            return employee1.getName().compareTo(employee2.getName());
-        }
-    };
-
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
+        Comparator<Employee> employeeComparator = new Comparator<>() {
+            @Override
+            public int compare(Employee employee1, Employee employee2) {
+                int compareAges = Integer.compare(employee1.getAge(), employee2.getAge());
+                if (compareAges != 0) {
+                    return compareAges;
+                }
+                return employee1.getName().compareTo(employee2.getName());
+            }
+        };
+
         Set<Employee> employeeSet = new TreeSet<>(employeeComparator);
         employeeSet.addAll(employees);
         return employeeSet;
