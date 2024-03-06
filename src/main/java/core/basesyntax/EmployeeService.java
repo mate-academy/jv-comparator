65e4cde9658272179249bc8e063f4c4aeae93849
Milosz <szwajkowski.milosz@gmail.com>
2024-03-06 18:47:35 +0100

    Solved jv-comparator task:
    Created comparator and implemented requested method

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,24 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
+    private static final Comparator<Employee> AGE_NAME_COMPARATOR = new Comparator<>() {
+        @Override
+        public int compare(Employee o1, Employee o2) {
+            if (o1.getAge() != o2.getAge()) {
+                return o1.getAge() - o2.getAge();
+            }
+            return o1.getName().compareTo(o2.getName());
+        }
+    };
+
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        TreeSet<Employee> sortedEmployees = new TreeSet<>(AGE_NAME_COMPARATOR);
+        sortedEmployees.addAll(employees);
+        return sortedEmployees;
     }
 }
