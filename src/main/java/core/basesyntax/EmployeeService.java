b9910eba6d6338f3f965c305c94265fa73e64280
Milosz <szwajkowski.milosz@gmail.com>
2024-03-10 19:10:42 +0100

    Moved comparator to its own class, changed comparison method for age to Integer.compare

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,18 +6,9 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-    private static final Comparator<Employee> AGE_NAME_COMPARATOR = new Comparator<>() {
-        @Override
-        public int compare(Employee o1, Employee o2) {
-            if (o1.getAge() != o2.getAge()) {
-                return o1.getAge() - o2.getAge();
-            }
-            return o1.getName().compareTo(o2.getName());
-        }
-    };
-
+    private static final Comparator<Employee> EMPLOYEE_COMPARATOR = new EmployeeByAgeAndNameComparator();
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        TreeSet<Employee> sortedEmployees = new TreeSet<>(AGE_NAME_COMPARATOR);
+        TreeSet<Employee> sortedEmployees = new TreeSet<>(EMPLOYEE_COMPARATOR);
         sortedEmployees.addAll(employees);
         return sortedEmployees;
     }
