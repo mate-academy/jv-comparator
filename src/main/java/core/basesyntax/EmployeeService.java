7701467e1e36d687adbe3653ddd0abadce58d802
Banderan <bartoszwojcik2004@gmail.com>
2024-05-06 19:42:40 +0200

    First solution

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,27 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
+    Comparator<Employee> comparator = new Comparator<Employee>() {
+        @Override
+        public int compare(Employee employee1, Employee employee2) {
+
+            int compareAge = Integer.compare(employee1.getAge(), employee2.getAge());
+            if (compareAge == 0) {
+                int compareName = employee1.getName().compareTo(employee2.getName());
+                return compareName;
+            }
+            return compareAge;
+        }
+    };
+
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Set<Employee> treeSetSort = new TreeSet<>(comparator);
+        treeSetSort.addAll(employees);
+        return treeSetSort;
     }
 }
