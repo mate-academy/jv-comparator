fe737da17b0b613a704e630b27c9518d4e2db699
Tomasz Baran <MarcusOfficial@tuta.io>
2024-06-13 10:26:46 +0200

    maven corrections

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,13 +6,6 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-    public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        Set<Employee> workers = new TreeSet<>(employeeComparator);
-        workers.addAll(employees);
-
-        return workers;
-    }
-
     private final Comparator<Employee> employeeComparator = (firstEmployee, secondEmployee) -> {
         int ageComparison = Integer.compare(firstEmployee.getAge(), secondEmployee.getAge());
         if (ageComparison != 0) {
@@ -20,4 +13,11 @@ public class EmployeeService {
         }
         return firstEmployee.getName().compareTo(secondEmployee.getName());
     };
+
+    public Set<Employee> getEmployByOrder(List<Employee> employees) {
+        Set<Employee> workers = new TreeSet<>(employeeComparator);
+        workers.addAll(employees);
+
+        return workers;
+    }
 }
