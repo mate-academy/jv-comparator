abe0a0d141b72d2a093327a79b42c6307c698a32
smagles <lesialist@icloud.com>
2024-07-15 20:45:14 +0200

    Create Comparator and Implement compare method for sorting by age and name

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,24 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Set<Employee> employeeSet = new TreeSet<>(employeeComparator());
+        employeeSet.addAll(employees);
+        return employeeSet;
+    }
+
+    private Comparator<Employee> employeeComparator() {
+        return (o1, o2) -> {
+            int result = o1.getAge() - o2.getAge();
+            if (result == 0) {
+                result = o1.getName().compareTo(o2.getName());
+            }
+            return result;
+        };
     }
 }
