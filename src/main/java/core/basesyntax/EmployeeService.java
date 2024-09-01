dc7afa192b967230e1b8c85b71ccb4d453019869
Oleksii <obabych1@stu.vistula.edu.pl>
2024-09-01 10:09:05 +0200

    commit task

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,27 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Comparator<Employee> employeeComparator = new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                int ageCompared = Integer.compare(o1.getAge(), o2.getAge());
+
+                if (ageCompared != 0) {
+                    return ageCompared;
+                }
+
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
+
+        Set<Employee> sortedEmployees = new TreeSet<>(employeeComparator);
+        sortedEmployees.addAll(employees);
+        return sortedEmployees;
     }
 }
