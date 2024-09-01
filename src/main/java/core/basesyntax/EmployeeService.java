4ccb8aa70d597c676d43af46324f940abdc1c313
Oleksii <obabych1@stu.vistula.edu.pl>
2024-09-01 10:12:50 +0200

    used lambda

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -4,13 +4,12 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        Comparator<Employee> employeeComparator = Comparator.comparingInt(Employee::getAge).thenComparing(Employee::getName);
-
-        Set<Employee> sortedEmployees = new TreeSet<>(employeeComparator);
-        sortedEmployees.addAll(employees);
-        return sortedEmployees;
+        return employees.stream().sorted(Comparator.comparingInt(Employee::getAge)
+                .thenComparing(Employee::getName))
+                .collect(Collectors.toCollection(TreeSet::new));
     }
 }
