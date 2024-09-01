95c7cfb568d06ed19d416d5c21f04199472832f1
Oleksii <obabych1@stu.vistula.edu.pl>
2024-09-01 10:11:20 +0200

    used lambda

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -7,18 +7,7 @@ import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        Comparator<Employee> employeeComparator = new Comparator<Employee>() {
-            @Override
-            public int compare(Employee o1, Employee o2) {
-                int ageCompared = Integer.compare(o1.getAge(), o2.getAge());
-
-                if (ageCompared != 0) {
-                    return ageCompared;
-                }
-
-                return o1.getName().compareTo(o2.getName());
-            }
-        };
+        Comparator<Employee> employeeComparator = Comparator.comparingInt(Employee::getAge).thenComparing(Employee::getName);
 
         Set<Employee> sortedEmployees = new TreeSet<>(employeeComparator);
         sortedEmployees.addAll(employees);
