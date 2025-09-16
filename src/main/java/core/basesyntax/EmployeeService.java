d695ee4883397c8cb37ef7f23d5728e59ef13db9
nowakjd <JedrzejNowak@protonmail.com>
2025-09-16 07:19:04 +0200

    implementing getEmployByOrder

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,25 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
+
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Comparator<Employee> employeeByAgeAndNameComparator = new Comparator<>() {
+            @Override
+            public int compare(Employee firstEmployee, Employee secondEmployee) {
+                int comparison = Integer.compare(firstEmployee.getAge(), secondEmployee.getAge());
+                if (comparison == 0) {
+                    return firstEmployee.getName().compareTo(secondEmployee.getName());
+                }
+                return comparison;
+            }
+        };
+        Set<Employee> result = new TreeSet<>(employeeByAgeAndNameComparator);
+        result.addAll(employees);
+        return result;
     }
 }
