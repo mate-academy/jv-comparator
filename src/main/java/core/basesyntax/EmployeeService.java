aa7c311ad752d7a8167141d357a64fc47d4deffb
Jakub Cybulski <jakub_cybulski@o2.pl>
2025-10-10 11:49:57 +0200

    Task Comparator full impl

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -2,9 +2,12 @@ package core.basesyntax;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
-    public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+    public Set<Employee> getEmployeeByOrder(List<Employee> employees) {
+        Set<Employee> employeesSet = new TreeSet<>(new EmployeeComparator());
+        employeesSet.addAll(employees);
+        return employeesSet;
     }
 }
