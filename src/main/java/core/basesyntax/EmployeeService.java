c11685189d71db97b41a235e72bf80cc80b9267c
Slawomir Pacek <slawomir_pacek@op.pl>
2025-11-28 15:25:53 +0100

    comparator

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -2,9 +2,20 @@ package core.basesyntax;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+
+        Set<Employee> sortedEmployees = new TreeSet<>((e1, e2) -> {
+            int ageCompare = Integer.compare(e1.getAge(), e2.getAge());
+            if (ageCompare != 0) {
+                return ageCompare; // młodszy pierwszy
+            }
+            return e1.getName().compareTo(e2.getName()); // alfabetycznie jeśli wiek taki sam
+        });
+
+        sortedEmployees.addAll(employees);
+        return sortedEmployees;
     }
 }
