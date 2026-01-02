fd5dbea9a57c921c23d7978121eebe4a99fcf008
Przemysław Ciżmiński <cizminski@interia.pl>
2026-01-02 20:59:57 +0100

    solution 2

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class EmployeeService {
-    public Set<Employee> getEmployByOrder(List<Employee> employees) {
+    public Set<Employee> getEmploeeByOrder(List<Employee> employees) {
         Comparator<Employee> employeeComparator = new Comparator<Employee>() {
             @Override
             public int compare(Employee o1, Employee o2) {
