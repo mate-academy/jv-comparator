422e1c89b126bba8371eb37d0c5369e9857b7c56
user <pawel.m.janik@gmail.com>
2026-07-22 14:49:56 +0200

    Task complete. Implemented getEmployByOrder method. Results are sorted first by age, then by name.

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,21 @@
 package core.basesyntax;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-public class EmployeeService {
-    public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+public class EmployeeService implements Comparator<Employee> {
+    @Override
+    public int compare(Employee employee1, Employee employee2) {
+        if (employee1.getAge() == (employee2.getAge())) {
+            return employee1.getName().compareTo(employee2.getName());
+        }
+        return Integer.compare(employee1.getAge(), employee2.getAge());
     }
+
+    public Set<Employee> getEmployByOrder(List<Employee> employees) {
+        Set<Employee> sortedEmployees = new TreeSet<>(this);
+        sortedEmployees.addAll(employees);
+        return sortedEmployees;
+    }
+
+
 }
