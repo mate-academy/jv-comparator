54bb1a6ca440b715856cc0b0aba8c7af0f1b337d
Maksim Abramovich <maks.abramovich.1995@mail.ru>
2026-09-13 18:05:34 +0200

    Add sorting logic to EmployeeService using age and name comparators

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,24 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        Comparator<Employee> employeeComparator = new Comparator<Employee>() {
+            @Override
+            public int compare(final Employee o1, final Employee o2) {
+                int ageComp = Integer.compare(o1.getAge(), o2.getAge());
+                if (ageComp != 0) {
+                    return ageComp;
+                }
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
+        TreeSet<Employee> employeesSort = new TreeSet<>(employeeComparator);
+        employeesSort.addAll(employees);
+        return employeesSort;
     }
 }
